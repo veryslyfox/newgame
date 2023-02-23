d7d397e1229abe020c2f257084212f5f36fcd27e
Avgust Zagorodnikov <zagavg@yandex.ru>
2023-02-23 18:29:03 +0300

    Update! 0.2!

--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
 ﻿enum Cell
 {
     Empty,
-    Player
+    Player,
+    Barrier,
+    Exit,
 }
 static class Program
 {
     public static Cell[,] _field = new Cell[30, 30];
     public static int _playerX, _playerY;
+    public static Random _rng = new();
+    private static string _error;
+
     static void Main()
     {
         Console.CursorVisible = false;
+        for (int i = 0; i < 300; i++)
+        {
+            _field[_rng.Next(30), _rng.Next(30)] = Cell.Barrier;
+        }
+        DrawField();
         while (true)
         {
-            ProcessLogic();
             ProcessInput();
+            ProcessLogic();
             DrawField();
         }
     }
@@ -24,45 +34,41 @@ static class Program
     private static void ProcessInput()
     {
         var key = Console.ReadKey(true);
-        void Clear()
-        {
-            _field[_playerX, _playerY] = Cell.Empty;
-        }
         switch (key.Key)
         {
             case ConsoleKey.W:
             case ConsoleKey.UpArrow:
-                if (_playerY != 0)
-                {
-                    Clear();
-                    _playerY--;
-                }
+                OffsetHandle(0, -1);
                 break;
             case ConsoleKey.S:
             case ConsoleKey.DownArrow:
-                if (_playerY != 0)
-                {
-                    Clear();
-                    _playerY++;
-                }
+                OffsetHandle(0, 1);
                 break;
             case ConsoleKey.A:
             case ConsoleKey.LeftArrow:
-                if (_playerY != 0)
-                {
-                    Clear();
-                    _playerX--;
-                }
+                OffsetHandle(-1, 0);
                 break;
             case ConsoleKey.D:
             case ConsoleKey.RightArrow:
-                if (_playerY != 0)
-                {
-                    Clear();
-                    _playerX++;
-                }
+                OffsetHandle(1, 0);
                 break;
         }
+    }
+    private static void OffsetHandle(int x, int y)
+    {
+        var xNext = _playerX + x;
+        var yNext = _playerY + y;
+        if (xNext < 0 || yNext < 0 || xNext > _field.GetLength(0) - 1 || yNext > _field.GetLength(1) - 1)
+        {
+            return;
+        }
+        if (_field[xNext, yNext] == Cell.Barrier)
+        {
+            return;
+        }
+        _field[_playerX, _playerY] = Cell.Empty;
+        _playerX = xNext;
+        _playerY = yNext;
     }
     private static void DrawField()
     {
@@ -80,10 +86,15 @@ static class Program
                     case Cell.Player:
                         symbol = '!';
                         break;
+                    case Cell.Barrier:
+                        symbol = '#';
+                        break;
+                    
                 }
                 Console.Write(symbol);
             }
             Console.WriteLine();
         }
+        Console.WriteLine(_error);
     }
 }
